75e7bef2bd93f34d76d41d7917d26f2c2260a1cc
Ondrej Keprt <you@example.com>
2021-11-14 21:10:44 +0100

    frgot

--- read_from_server.h
+++ read_from_server.h
@@ -5,6 +5,14 @@
 
 #include <iostream>
 #include <stdexcept>
+#include "functions.h"
+#include "comunication.h"
+#include <sys/statvfs.h>
+#include <exception>
+#include <string>
+#include <cstdlib>
+#include <sys/ioctl.h>
+#include <net/if.h>
 
 int read_from_server_main(std::string &path,int timeout,int size,bool b_multicast,std::string&mode, std::string &ip,std::string &port);
 
